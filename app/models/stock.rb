ebfea51261b4a7859465c344a1abaf324e23cedf
Matt Peters <matt78180@gmail.com>
2020-03-15 22:09:22 -0400

    Update stock.rb

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,5 +2,5 @@ class Stock < ApplicationRecord
 	belongs_to :user
 	validates :ticker, presence: true
 	validates :ticker, format:{ without: /\s/}
-	validates_uniqueness_of   :ticker, scope: :ticker
+	validates_uniqueness_of   :ticker, scope: :user_id
 end
